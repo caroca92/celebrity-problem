e767ec791474113f8bea4689b6bf326ff1e55635
Carlos Romero <carlose.romero@softtek.com>
2021-01-18 18:24:10 -0600

    Added Map to count while inspecting the array

--- src/main/java/Algorithm.java
+++ src/main/java/Algorithm.java
@@ -1,4 +1,6 @@
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -8,18 +10,30 @@ public class Algorithm {
     // known by everyone but he/she doesn't know anybody
     public String findingFamousPeople(List<Person> team){
         //operations
+        Map<Person, Integer> countingPersonAppearance = new HashMap<>();
+
         List<Person> justOneFamousAllowed = team
                 .stream()
-                .filter(person -> person.getPeopleThatKnow() == null || person.getPeopleThatKnow().isEmpty())
+                .filter(person -> {
+                    if(person.getPeopleThatKnow() == null || person.getPeopleThatKnow().isEmpty()){
+                        countingPersonAppearance.put(person, 0);
+                        return true;
+                    }
+                    person.getPeopleThatKnow().forEach(peopleWhoKnow -> {
+                        if(countingPersonAppearance.get(peopleWhoKnow) == null){
+                            countingPersonAppearance.put(peopleWhoKnow, 0);
+                        }else{
+                            countingPersonAppearance.put(peopleWhoKnow, countingPersonAppearance.get(peopleWhoKnow) + 1);
+                        }
+                    });
+                    return false;
+                })
                 .collect(Collectors.toList());
         Boolean isCandidateToBeCelebrityACelebrity = Boolean.FALSE;
         if(justOneFamousAllowed.size() > 1 || justOneFamousAllowed.size() < 1){
             return "No celebrity is here";
         }else {
-            isCandidateToBeCelebrityACelebrity = team
-                    .stream()
-                    .filter(person -> person != justOneFamousAllowed.get(0))
-                    .allMatch(person -> person.getPeopleThatKnow().contains(justOneFamousAllowed.get(0)));
+            isCandidateToBeCelebrityACelebrity = countingPersonAppearance.get(justOneFamousAllowed.get(0)).equals(team.size() - 1);
         }
 
         return (isCandidateToBeCelebrityACelebrity ? justOneFamousAllowed.get(0).toString() : "No celebrity is here");
